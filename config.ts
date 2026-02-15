519991ed77b31a61b9aaf0ada50f8aa912b5f265
huyxchecker <pillowgotnowoamn@gmail.com>
2026-02-16 00:37:43 +0700

    discord emplementations

--- config.ts
+++ config.ts
@@ -4,7 +4,7 @@ import { http } from 'wagmi';
 
 // NOTE: In a production environment, you should obtain a WalletConnect Project ID from https://cloud.walletconnect.com
 // and place it in your .env file as VITE_WALLET_CONNECT_PROJECT_ID
-export const walletConnectProjectId = import.meta.env.VITE_WALLET_CONNECT_PROJECT_ID as
+export const walletConnectProjectId = "10630c7967a588bedf722a6a18aeefb4" as
   | string
   | undefined;
 
@@ -12,11 +12,11 @@ const mainnetRpcUrl = (import.meta.env.VITE_MAINNET_RPC_URL as string | undefine
 
 export const config = walletConnectProjectId
   ? getDefaultConfig({
-      appName: 'TokenVault App',
-      projectId: walletConnectProjectId,
-      chains: [mainnet],
-      transports: {
-        [mainnet.id]: http(mainnetRpcUrl),
-      },
-    })
+    appName: 'TokenVault App',
+    projectId: walletConnectProjectId,
+    chains: [mainnet],
+    transports: {
+      [mainnet.id]: http(mainnetRpcUrl),
+    },
+  })
   : null;
